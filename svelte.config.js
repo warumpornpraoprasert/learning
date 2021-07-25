6816016188073a0b0a985cc25b23c308c80ee74e
warumpornpraoprasert <warumporn@protonmail.com>
2021-07-25 20:46:47 +0700

    build again

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
   kit: {
     paths: { 
       base: '/learning', 
-      assets: '/.' 
+      assets: '/learning' 
     },
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
